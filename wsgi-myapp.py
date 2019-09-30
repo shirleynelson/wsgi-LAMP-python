80ed8d0c186c1931458e393068ed8d5c6315ea1e
shirleynelson <52511947+shirleynelson@users.noreply.github.com>
2019-09-29 21:07:12 -0400

    Added Link to GitHub

--- wsgi-myapp.py
+++ wsgi-myapp.py
@@ -1,10 +1,9 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello World!'
+    output = b'Hello World!<br><a href="https://github.com/shirleynelson/wsgi-LAMP-python">GitHub LAMP Source</a>'
 
-    response_headers = [('Content-type', 'text/plain'),
+    response_headers = [('Content-type', 'text/html'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)
 
     return [output]
-    
